96039934ebae6088b60fc95df1ea2c20f8344270
christian <cw1521@jagmail.southalabama.edu>
2019-12-25 14:53:33 -0600

    successful compile heroku test

--- server.ts
+++ server.ts
@@ -49,12 +49,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(helmet());
 
-app.use("/p99atlasdb-api", express.static("public"));
+//app.use("/p99atlasdb-api");
 
 app.use(cors());
 
 
-app.use(function(req, res, next) {
+app.use("/p99atlasdb-api", function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
   res.header("Access-Control-Allow-Headers", "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json");
