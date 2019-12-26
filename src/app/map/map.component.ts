90f385d510e538fa237c291a78d42f2f2fddade2
christian <cw1521@jagmail.southalabama.edu>
2019-12-25 21:53:08 -0600

    heroku test favicon

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -21,7 +21,7 @@ export class MapComponent implements OnInit {
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
       var temp;
-      this.mapService.getMapByName(params.get('zoneName'), params.get('mapName'))
+      this.mapService.getMapByName(String(params.get('zoneName')), String(params.get('mapName')))
       .subscribe(map => {
         this.map = map["data"];
         this.map.continent = this.map.continent[0].toUpperCase() + this.map.continent.slice(1);
