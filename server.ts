641cf7100762b4d8cceb53d1c1733299249b0c13
christian <cw1521@jagmail.southalabama.edu>
2019-12-25 19:17:09 -0600

    heroku test favicon

--- server.ts
+++ server.ts
@@ -69,7 +69,7 @@ app.use("/p99atlasdb-api", function(req, res, next) {
 
 
 // uncomment after placing your favicon in /public
-app.use(favicon(join(process.cwd(), "dist/browser/favicon.ico")));
+//app.use(favicon(join(process.cwd(), "dist/browser/favicon.ico")));
 
 
 // Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
