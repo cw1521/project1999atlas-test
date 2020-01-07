14620f17ac3d516ffa0e526c016d70dfe0f68808
christian <cw1521@jagmail.southalabama.edu>
2020-01-06 18:02:27 -0600

    test environment vars

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   MONGODB_URI: process.env.MONGODB_URI,
-  baseUrl: 'https://www.project1999atlas.com/'
+  baseUrl: 'https://p99atlas-dev.herokuapp.com'
 };
