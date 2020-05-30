d7f60a670baaac56481a5b9493a1e8b3fd4a429f
cw1521 <cw1521@jagmail.southalabama.edu>
2020-05-29 20:13:38 -0500

    https fix attempt

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   MONGODB_URI: `mongodb://localhost:27017/p99`,
-  baseUrl: 'https://167.172.255.184:3500'
+  baseUrl: 'www.project1999atlas.com:3500'
 };
